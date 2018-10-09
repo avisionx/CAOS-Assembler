b64e66b888a7a2fc12a5b87e833950d9112fb9c6
Avi Garg <32339251+avisionx@users.noreply.github.com>
2018-10-09 19:59:17 +0530

    File Processing

--- assembler.py
+++ assembler.py
@@ -1,13 +1,36 @@
-# Variable Setup
-input_file = "sample-input.txt"
+# Imports
+import re
 
+# Variable Setup
+input_file_name = "sample-input.txt"
+processed_file_name = "processedFile.txt"
+
+# Setting up file for processing
+def setup_file():
+	
+	inputFile = open(input_file_name, "r")
+	processedList = [] 
+	
+	for line in inputFile:
+		convertTabsToSpace = line.replace('	', ' ')
+		toSingleSpace = re.sub("\s\s+" , " ", convertTabsToSpace)
+		convertSpaceToCommas = toSingleSpace.replace(' ', ',')
+		lineWithEndLine = convertSpaceToCommas.split(',')
+		lineWithEndLine[-1] = lineWithEndLine[-1].split()[0]
+		finalLine = lineWithEndLine
+		processedList.append(finalLine)
+
+	inputFile.close()
+
+	return processedList
 
 # Main Function
 def main():
-	file = open(input_file, "r")
-	for line in file:
-		a = line.split()
-		print(a)
+	
+	procList = setup_file()
+	
+	for line in procList:
+		print(line)
 
 if __name__ == '__main__':
 	main()
